72e1f92d2493c7beee1dac2357e5cc9d8ea7da6a
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-10-17 21:29:33 +0300

    Next

--- Assets/1Game/Scripts/ParticleSystemController.cs
+++ Assets/1Game/Scripts/ParticleSystemController.cs
@@ -5,7 +5,7 @@ using UnityEngine.Events;
 
 public class ParticleSystemController : MonoBehaviour
 {
-    [SerializeField] private List<GrabMashine> _grabMashine;
+    [SerializeField] private List<GrabMashine> _grabMashines;
     private ParticleSystem _particleSystem;
 
     private Vector3 _velosityParticle;
@@ -13,34 +13,52 @@ public class ParticleSystemController : MonoBehaviour
     private float _minSizeParticle;
 
     public UnityAction<ParticleSystem.Particle> CatchParticle;
+    private List <Collider> _leavesTanks;
 
 
     private void Start()
     {
         float maxVelocity = 100f;
+        
         _velosityParticle = new Vector3(maxVelocity, maxVelocity, maxVelocity);
         _stepSizeDown = 0.1f;
         _minSizeParticle = 0.3f;
         _particleSystem = GetComponent<ParticleSystem>();
+        _leavesTanks = new List<Collider>();
+
+        for (int i = 0; i < _grabMashines.Count; i++)
+        {
+
+            Collider colliderLeavesTank = _grabMashines[i].GetComponentInChildren<LeavesTank>().GetComponent<Collider>();
+            _leavesTanks.Add(colliderLeavesTank);
+
+
+        }
+        
     }
 
     private void OnParticleCollision(GameObject other)
     {
-        if (other.TryGetComponent<GrabMashine>(out GrabMashine grabMashine))
+        if (other.TryGetComponent<LeavesTank>(out LeavesTank leavesTank))
         {
 
             List<ParticleSystem.Particle> inside = new List<ParticleSystem.Particle>();
             int numInside = _particleSystem.GetTriggerParticles(ParticleSystemTriggerEventType.Inside, inside);
 
-            for (int i = 0; i < _grabMashine.Count; i++)
+            for (int i = 0; i < _grabMashines.Count; i++)
             {
                 for (int j = 0; j < numInside; j++)
                 {
                     ParticleSystem.Particle particle = inside[j];
+                    if (_leavesTanks)
+)
+                    {
+
+                    }
 
                     if (particle.startSize < _minSizeParticle)
                     {
-                        _grabMashine[i].OnGetParticle();
+                        _grabMashines[i].OnGetParticle();
                         particle.velocity = _velosityParticle;
                     }
                     else
--- Assets/1Game/Scripts/TrashBagIdle.cs
+++ Assets/1Game/Scripts/TrashBagIdle.cs
@@ -4,24 +4,12 @@ using UnityEngine;
 
 public class TrashBagIdle : MonoBehaviour
 {
-     private GrabMashine _grabMashine;
     private MeshRenderer _meshRenderer; 
 
     private void Start()
     {
-        _grabMashine = GetComponent<GrabMashine>();
         _meshRenderer = GetComponent<MeshRenderer>();
         _meshRenderer.enabled = false;
-    }
-
-    private void OnEnable()
-    {
-        _grabMashine.StartFillng += OnStartFilling;
-    }
-
-    private void OnDisable()
-    {
-        _grabMashine.StartFillng -= OnStartFilling;
     }
 
     private void OnStartFilling(bool isWork)
