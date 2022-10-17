361093fa780ca904917c5415ec6f3e62665d8e52
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-10-17 12:54:58 +0300

    update

--- Assets/1Game/Scripts/GrabMashine.cs
+++ Assets/1Game/Scripts/GrabMashine.cs
@@ -6,22 +6,32 @@ using UnityEngine.Events;
 public class GrabMashine : MonoBehaviour
 {
     [SerializeField]  private ParticleSystemController _particleSystem;
-    [SerializeField]  private TrashBagIdle _trashBagIdle;
-
-    private Vector3 _velosityParticle;
+    [SerializeField]  private TrashBag _trashBag;
+    private GrabMashine _grabMashine;
+     private TrashBagIdle _trashBagIdle;
+     
     private Vector3 _trashBagStartSize;
-    private Vector3 _tempTrashBagStartSize;
+    private Vector3 _trashBagStartPosition;
+    private Vector3 _tempSize;
+
     private float _maxSize;
     private float _stepUpSize;
+    private float _maxStepUp;
     private float _stepSizeDown;
     private float _minSizeParticle;
+
     //private List <ParticleSystem.Particle> _particle;
     private int _quantityCathcedParticle;
     private bool _isWork;
     private int _quantityUpSize;
     private int _tempQuantityUpSize;
+
     private int _quantityStepUp;
+    private int _tempQuantityStepUp;
+
     private int _quantityAllStepUp;
+
+    private WaitForSeconds _waitForSeconds;
 
 
 
@@ -29,15 +39,55 @@ public class GrabMashine : MonoBehaviour
 
     private void Start()
     {
+        _grabMashine = GetComponentInParent<GrabMashine>();
+        float waiteTime=0.2f;
+        _waitForSeconds = new WaitForSeconds(waiteTime);
+        _trashBagIdle = GetComponentInChildren<TrashBagIdle>();
         _trashBagStartSize = _trashBagIdle.transform.localScale;
-        _tempTrashBagStartSize = _trashBagStartSize;
+        _trashBagStartPosition= _trashBagIdle.transform.position;
+        _tempSize = _trashBagStartSize;
         _maxSize = 1.13f;
-        _stepUpSize = 0.1f;
+        _maxStepUp=0.2f;
+        _stepUpSize = 0.05f;
            _quantityUpSize = 50;
         _tempQuantityUpSize = _quantityUpSize;
         _quantityStepUp = 4;
         StartCoroutine(CountParticle());
     }
+
+   private IEnumerator ChangeSize()
+    {
+        
+        while (_tempSize.x <= _trashBagStartSize.x+ _maxStepUp)
+        {
+            _trashBagIdle.transform.localScale = new Vector3 (_tempSize.x + _stepUpSize, _tempSize.y + _stepUpSize, _tempSize.z + _stepUpSize);
+            _tempSize = _trashBagIdle.transform.localScale;
+            yield return _waitForSeconds;
+        }
+
+        _tempQuantityStepUp++;
+        _quantityAllStepUp--;
+
+        if (_tempQuantityStepUp==_quantityStepUp )
+        {
+            _tempQuantityStepUp = 0;
+            CreateTrashBag();
+        }
+        else
+        {
+            _quantityStepUp++;
+        }
+       
+
+    }
+
+    private void CreateTrashBag()
+    {
+        TrashBag _newTrashBag = Instantiate(_trashBag, _trashBagStartPosition, Quaternion.identity);
+        _newTrashBag.transform.SetParent()
+    }
+
+
 
 
     private IEnumerator CountParticle()
@@ -61,14 +111,7 @@ public class GrabMashine : MonoBehaviour
     }
 
 
-    private IEnumerator ChangeSize()
-    {
-        while (_trashBagStartSize.x<)
-        {
-
-        }
-        _trashBagStartSize
-    }
+    
     
 
 
