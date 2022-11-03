c1f82c616aa48317a794b9bb9dbe4023ccbc1b83
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-11-03 10:25:15 +0300

    Change parkPlace

--- Assets/1Game/Scripts/UI/UIParkStat.cs
+++ Assets/1Game/Scripts/UI/UIParkStat.cs
@@ -1,24 +1,26 @@
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using System.Collections;
 
 public class UIParkStat : MonoBehaviour
 { 
     [SerializeField]private Cart _cart;
-
+    private WaitForSeconds _waitForSeconds;
     private float _maxValue;
     private float _minValue;
     private Slider _slider;
 
     private void Start()
     {
+        
+
         _minValue = 0;
         _slider = GetComponent<Slider>();
         _maxValue = _cart.Time;
         _slider.maxValue = _maxValue;
         _slider.minValue = _minValue;
         _slider.value = _minValue;
-
     }
 
     private void OnEnable()
@@ -34,11 +36,21 @@ public class UIParkStat : MonoBehaviour
     private void OnStartMove()
     {
         _slider.DOValue(_maxValue, (_maxValue+ _maxValue), false);
-
     }
 
     public void Initialization(float time)
     {
         _maxValue = time;
+    }
+
+    private IEnumerator WaiteEndMove()
+    {
+        while (_slider.value < _maxValue)
+        {
+            yield return null;
+        }
+
+        _slider.value = 0;
+        StopCoroutine(WaiteEndMove());
     }
 }
